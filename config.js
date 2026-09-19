7f092328523a062fee26c8c6aa9d1b6b03c5f532
Steve <stephenmatthew283@gmail.com>
2026-09-19 20:23:11 +0100

    Fix this

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
-window.FIELDNOTES_CONFIG = {
-  supabaseUrl: 'https://tyicduuijghugeztrmmj.supabase.co',
-  supabaseAnonKey: 'sb_publishable_xYH_3Mnn4rcfnKym7RVyMw_X2FASqON',
-}
+
+
+window.ENV = {
+  SUPABASE_URL: 'https://tyicduuijghugeztrmmj.supabase.co', // Replace with your actual Supabase project URL if different
+  SUPABASE_ANON_KEY: 'sb_publishable_xYH_3Mnn4rcfnKym7RVyMw_X2FASqON', // Insert your Supabase anon key here
+  GOOGLE_CLIENT_ID: '171352585547-ep44q92u07gm2s4r6742ujnintf1hqoc.apps.googleusercontent.com'
+};
